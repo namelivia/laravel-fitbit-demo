cb5054b1b390acfb4a0518ceca1a377c90616f24
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-06-29 14:50:24 +0200

    Setup a simple demo

--- config/app.php
+++ config/app.php
@@ -226,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Fitbit' => Namelivia\Fitbit\Laravel\Facades\Fitbit::class,
 
     ],
 
